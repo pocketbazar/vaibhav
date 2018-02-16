382558f76b0ac106b105e9c7d11c12d6bdb6219f
pocketbazar <vaib.d.goyal@gmail.com>
2018-02-16 18:10:03 +0530

    Major Update (Product Page)

--- PocketBazar/js/colorflash.js
+++ PocketBazar/js/colorflash.js
@@ -1,5 +1,5 @@
 function flash() {
     var text = document.getElementById('flashtext');
-    text.style.color = (text.style.color=='red') ? 'blue':'red';
+    text.style.color = (text.style.color=='red') ? '#3498db':'red';
 }
 var clr = setInterval(flash, 1000);
